8100aeaa6bcad6ae5cfec9b1dfaedd33445ea792
Eguchi-Kd <student7653@gmail.com>
2024-10-18 22:18:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 
-const apiKey = process.env.API_KEY;
-const clientId = process.env.CLIENT_ID;
+const spreadsheet_id = process.env.SPREADSHEET_ID;
 
-console.log("API Key:", apiKey);
-console.log("Client ID:", clientId);
+console.log("SPREADSHEET_ID:", spreadsheet_id);
 
 // ここで API 呼び出しなどの処理を行います
