dbc42d79783debd35c7285f11a25852f9f7b6687
Eguchi-Kd <student7653@gmail.com>
2024-10-18 12:32:04 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # secret_fileファイルは公開しない（gitignore）ファイルだから、ファイルがなくてロードされなければパスする
 try:
-    from .secret_file.py import API_KEY, API_SECRET, ACCESS_KEY, ACCESS_SECRET
+    from .secret_file.py import API_KEY, CLIENT_ID
 except ImportError:
     pass
 
@@ -10,6 +10,4 @@ import os
 # os.environを使えばこのプログラム上でのみ働く環境変数として設定できる（OSの環境には影響しない）
 if not DEBUG:
     os.environ['API_KEY']       = API_KEY
-    os.environ['API_SECRET']    = API_SECRET
-    os.environ['ACCESS_KEY']    = ACCESS_KEY
-    os.environ['ACCESS_SECRET'] = ACCESS_SECRET
+    os.environ['CLIENT_ID']     = CLIENT_ID
